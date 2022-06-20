496f250bc94401e401aa4b566450e574e42e6aa3
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-06-20 10:46:01 +0600

    another get api is performed

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const port =process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-    res.send('Server startet sucessfully');
+    res.send('Server startet sucessfully with mon');
+});
+
+app.get('/blog',(req,res)=>{
+    res.send('Haha its a blog');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('hoho you are seeing about me haaha');
 });
 
 app.listen(port,()=>{
